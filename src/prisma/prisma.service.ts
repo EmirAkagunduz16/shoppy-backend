9a7309bfa004d7a9ae263b7b3515d0ceb35e9148
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-12-02 15:20:28 +0300

    feat: update Prisma configuration, add bcrypt for password hashing, and enhance user creation logic

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'src/generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
+    // 1. PostgreSQL bağlantı havuzunu (Pool) oluştur
+    const connectionString = `${process.env.DATABASE_URL}`;
+
+    const pool = new Pool({
+      connectionString,
     });
+
+    // 2. Prisma Adapter'ı oluştur
+    const adapter = new PrismaPg(pool);
+
+    // 3. super() ile adapter'ı PrismaClient'a gönder
     super({ adapter });
+  }
+
+  async onModuleInit() {
+    // Adapter kullanıldığında $connect çağrısı bazen opsiyoneldir
+    // ama bağlantıyı başlatmak için burada tutabiliriz.
+    await this.$connect();
   }
 }
